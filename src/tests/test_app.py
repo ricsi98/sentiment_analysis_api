57b319b24466a0f025205b2c99c61a90be688f0d
krichard98 <kiss.richard@mkb.hu>
2023-10-28 12:38:53 +0200

    Add test annotations

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -5,12 +5,14 @@ client = TestClient(app)
 
 
 def test_health():
+    """We expect the response for the GET /health endpoint to return status: up"""
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json()["status"] == "up"
 
 
 def test_metadata():
+    """We expect the response for the GET /metadata endpoint to contain certain keys"""
     response = client.get("/metadata")
     assert response.status_code == 200
     json = response.json()
@@ -19,6 +21,7 @@ def test_metadata():
 
     
 def test_sentiment():
+    """We expect that the sentiment of a happy sentence is positive"""
     response = client.post("/predict", content='{"text": "I am very happy!"}', headers={"Content-Type": "application/json"})
     assert response.status_code == 200
     assert response.json()["sentiment"] == "positive"
--- src/tests/test_vader.py
+++ src/tests/test_vader.py
@@ -5,6 +5,10 @@ from vader import VaderSingleton
 
 
 def test_vader_download_fallback():
+    """
+    The Vader implementation looks for available vader_lexicon package. If it is not found it tries downloading it.
+    We expect that it is able to analyze the sentiment even if we first remove the necessary files.
+    """
     ptr = nltk.data.find("sentiment/vader_lexicon.zip")
     os.unlink(str(ptr))
     try:
@@ -16,5 +20,6 @@ def test_vader_download_fallback():
     
 
 def test_vader():
+    """We expect that the sentiment analyizer labels a happy sentence positive"""
     sentiment, _ = VaderSingleton().analyze_sentiment("I am very happy")
     assert sentiment == "positive"
