3f663c484f3bd6a6be535cc4728384656f1bcab4
krichard98 <kiss.richard@mkb.hu>
2023-10-28 12:17:48 +0200

    Remove print stmnt

--- src/logconfig.py
+++ src/logconfig.py
@@ -3,18 +3,23 @@ import os
 
 _LOG_FILE = os.getenv(
     "LOG_PATH",
-    os.path.join(os.path.dirname(__file__), "../logs")
+    os.path.join(os.path.dirname(__file__), "logs")
 )
-
-print(_LOG_FILE)
 
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(_LOG_FILE)
+
+    file_handler = logging.FileHandler(_LOG_FILE)
+    stream_handler = logging.StreamHandler()
+
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
     return logger
